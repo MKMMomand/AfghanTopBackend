0f0b612f3515c8fe2cff30089228e5aab697cd25
MKMMomand <mkm.khalidmomand@gmail.com>
2026-04-13 15:27:48 -0700

    bug fix for import

--- config/urls.py
+++ config/urls.py
@@ -6,8 +6,8 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
+from apps.providers.views import get_server_ip
 
-from providers.views import get_server_ip
 @api_view(["GET"])
 @permission_classes([AllowAny])
 def health(request):
