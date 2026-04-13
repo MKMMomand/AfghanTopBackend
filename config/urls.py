fa908ebfcba34e6ad3a1d3fe48310788bc3e9c86
MKMMomand <mkm.khalidmomand@gmail.com>
2026-04-13 15:23:27 -0700

    server-ip is not defined issue solved

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 
+from providers.views import get_server_ip
 @api_view(["GET"])
 @permission_classes([AllowAny])
 def health(request):
